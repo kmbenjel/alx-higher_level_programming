d36c3e7a9fe5914845669ee5f940c4c1ff5f3aa8
Khalid Benjelloun <khalidmailing@gmail.com>
2024-05-21 09:43:46 +0100

    edit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+sp = str.split()
+print(" ".join([sp[5], sp[6], sp[-4], sp[0]]))
