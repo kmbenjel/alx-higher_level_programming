0757361d5eaf2750408aca7dac906aeef6479a61
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-18 14:43:56 -0700

    edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log('Argument found');
+if (!process.argv[2]) {
+  console.log('No argument');
 } else {
-	if (process.argv[3]) { console.log('Arguments found'); } else { console.log('No argument') };
+	if (!process.argv[3]) { console.log('Argument found'); } else { console.log('Arguments found') };
 }
