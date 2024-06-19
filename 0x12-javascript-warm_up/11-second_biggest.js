6a9721078f572d51b2b877cfb47ccdbb72f2ab42
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-18 19:31:02 -0700

    edit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const array = process.argv.slice(2);
+const array = process.argv.slice(2).map(Number);
 const c = array.length;
 
-console.log(c > 1 ? array.sort().reverse()[1] : 0);
+console.log(c > 1 ? array.sort((a, b) => a - b)[c - 2] : 0);
