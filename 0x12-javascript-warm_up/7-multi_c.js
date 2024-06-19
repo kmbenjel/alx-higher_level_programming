0f75b7e1d922bd03c3341934c2da0803fa26eb10
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-18 18:07:51 -0700

    edit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-let n = process.argv[2];
-function repeat(n)
-{
-	for(let i = 0; i < n; i++) {console.log('C is fun');}
+const n = process.argv[2];
+function repeat (n) {
+  for (let i = 0; i < n; i++) { console.log('C is fun'); }
 }
 Number.isNaN(Number(n)) ? console.log('Missing number of occurrences') : repeat(n);
