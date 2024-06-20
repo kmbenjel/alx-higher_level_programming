5ccedf903aa8bcaea6bdea649fd70bcf419d267b
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-19 19:05:26 -0700

    edit

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
