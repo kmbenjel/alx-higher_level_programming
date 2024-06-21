e39632d3bb5709e6fdb22deaf3afaf1df0653615
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-20 20:02:38 -0700

    edit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Sq = require('./5-square.js');
+
+class Square extends Sq {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  printC () {
+    for (let i = 0; i < this.size; i++) {
+      console.log('C'.repeat(this.size));
+    }
+  }
+
+  charPrint (c) {
+    c ? this.printC() : this.print();
+  }
+}
+
+module.exports = Square;
