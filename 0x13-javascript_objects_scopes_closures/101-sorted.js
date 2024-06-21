4c0592b57455deca17628aa0e17710dd5e299c51
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-21 04:30:11 -0700

    edit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict
+
+occurrences = [...new Set(Object.values(dict).map(String))]
+
+const idsByOccurrences = function (num) {
+  return Object.keys(dict).filter(key => dict[key] === num);
+}
+
+const _new = occurrences.reduce((acc, key) => {
+  acc[key] = idsByOccurrences(Number(key));
+  return acc;
+  }, {});
+
+console.log(_new)
