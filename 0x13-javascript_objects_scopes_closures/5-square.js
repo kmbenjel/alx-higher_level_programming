17dc9e11c32d2175d1ef9fe1aaaedefa7d93240f
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-20 17:40:32 -0700

    edit

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,36 @@
 #!/usr/bin/node
 
-import Rectangle from './4-rectangle.js';
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
     this.size = size;
   }
 }
+
 module.exports = Square;
