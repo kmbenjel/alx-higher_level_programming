9e21cc99c8d70d5107820748fc03097a58482443
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-20 22:25:59 -0700

    edit

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-exports.esrever = function (list) { return list.reverse(); };
+exports.esrever = function (list) {
+  const max = list.length;
+  const newlist = [];
+  for (let i = 0; i < max; i++) { newlist[i] = list[max - i - 1]; }
+  return newlist;
+};
